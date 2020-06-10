ff624e343480d459d8f4cca28555a62bd550964e
jpham <mahp.jp@gmail.com>
2020-06-10 13:10:32 +0800

    version bump

--- notifications/__init__.py
+++ notifications/__init__.py
@@ -8,6 +8,6 @@
 """
 
 # PEP 386-compliant version number: N.N[.N]+[{a|b|c|rc}N[.N]+][.postN][.devN]
-__version__ = '2.0'
+__version__ = '2.1'
 
 default_app_config = 'notifications.apps.Config'  # pylint: disable=invalid-name
